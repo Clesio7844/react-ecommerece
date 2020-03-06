ee6207e37db1569e178b4c9d4f9ab834ff0d61fa
david <daviddecastro42@live.co.uk>
2020-03-06 23:48:23 +0000

    update the manu-items to navigate withRouter HOC high order component

--- clothing-app/src/App.js
+++ clothing-app/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homapage/homepage.component.';
 
 import './App.css';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
