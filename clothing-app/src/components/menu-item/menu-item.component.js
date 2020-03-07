de9e88dbeb855f6e68ad914976ea7d4c931a089f
david <daviddecastro42@live.co.uk>
2020-03-07 13:31:38 +0000

    updating our app with a shop page, collection preview component and collection item component

--- clothing-app/src/components/menu-item/menu-item.component.js
+++ clothing-app/src/components/menu-item/menu-item.component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+
 import './menu-item.style.scss';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
@@ -8,7 +9,7 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
     onClick={() => history.push(`${match.url}${linkUrl}`)}
   >
     <div
-      className='background-img'
+      className='background-image'
       style={{
         backgroundImage: `url(${imageUrl})`
       }}
